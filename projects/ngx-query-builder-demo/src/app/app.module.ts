b197d3484ef5fbdd546155a5813583644501ff7f
eisnerw <eisnerw@hotmail.com>
2025-07-11 13:55:49 -0400

    fix: import standalone edit dialog

--- projects/ngx-query-builder-demo/src/app/app.module.ts
+++ projects/ngx-query-builder-demo/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { QueryBuilderModule } from 'ngx-query-builder';
+// The EditRulesetDialogComponent is a standalone component. It should be
+// imported rather than declared to avoid NG6008 errors.
+import { EditRulesetDialogComponent } from './edit-ruleset-dialog.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    QueryBuilderModule
+    QueryBuilderModule,
+    EditRulesetDialogComponent
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
